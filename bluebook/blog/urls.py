88f462c92e7db6d585b0a460b5ff47c1928aac2e
gyusun <gedit@kakao.com>
2020-04-19 22:57:14 +0900

    third commit

--- bluebook/blog/urls.py
+++ bluebook/blog/urls.py
@@ -8,8 +8,10 @@ urlpatterns = [
     path('post/', views.PostLV.as_view(), name='post_list'), #/blog/post/
     re_path(r'^post/(?P<slug>[-\w]+)/$', views.PostDV.as_view(), name='post_detail'), #/blog/post/django-example/
     path('archive/', views.PostAV.as_view(), name='post_archive'), #/blog/archive/
-    path('archive/<int:year>/', views.PostYAV.as_view(), name='post_year_archive'), #/blog/archive/2020/    
+    path('archive/<int:year>/', views.PostYAV.as_view(), name='post_year_archive'), #/blog/archive/2020/
     path('archive/<int:year>/<str:month>/', views.PostMAV.as_view(), name='post_month_archive'), #/blog/archive/2020/nov/
     path('archive/<int:year>/<str:month>/<int:day>/', views.PostDAV.as_view(), name='post_day_archive'), #/blog/archive/2020/nov/10/
     path('archive/today/', views.PostTAV.as_view(), name='post_today_archive'), #/blog/archive/today/
+    path('tag/', views.TagCloudTV.as_view(), name='tag_cloud'),
+    path('tag/<str:tag>/', views.TaggedObjectLV.as_view(), name='tagged_object_list')
 ]
