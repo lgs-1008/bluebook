a541b84e42d9ae0342524ee6d27bf3a1cfea0c31
gyusun <gedit@kakao.com>
2020-04-21 23:30:45 +0900

    5th commit

--- bluebook/mysite/settings.py
+++ bluebook/mysite/settings.py
@@ -131,3 +131,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 TAGGIT_CASE_INSENSITIVE = True
 TAGGIT_LIMIT = 50
+
+DISQUS_SHORTNAME = 'my-django-site'
+DISQUS_MY_DOMAIN = 'http://127.0.0.1:8000'
