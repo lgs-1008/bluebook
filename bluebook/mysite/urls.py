d80f1f656de6b37a083c723be4e318d6571d7b02
gyusun <gedit@kakao.com>
2020-04-29 23:42:45 +0900

    13th commit

--- bluebook/mysite/urls.py
+++ bluebook/mysite/urls.py
@@ -17,16 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from mysite.views import HomeView
-from mysite.views import UserCreateView, UserCreateDoneTV
+from mysite import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/register/', UserCreateView.as_view(), name='register'),
-    path('accounts/register/done/', UserCreateDoneTV.as_view(), name='register_done'),
-    path('', HomeView.as_view(), name='home'),
+    path('accounts/register/', views.UserCreateView.as_view(), name='register'),
+    path('accounts/register/done/', views.UserCreateDoneTV.as_view(), name='register_done'),
+    path('', views.HomeView.as_view(), name='home'),
     path('bookmark/', include('bookmark.urls')),
     path('blog/', include('blog.urls')),
     path('photo/', include('photo.urls')),
